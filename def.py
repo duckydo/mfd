4f86cace8af3be2af75036b3f3a348dfd2da5995
duckydo <donaldjbaptiste@gmail.com>
2023-09-12 15:37:24 +0200

    grpd

---
+++ def.py
@@ -0,0 +1,16 @@
+def addition(a,b):
+    return a+b
+
+def soustraction(a,b):
+    return a-b
+
+def multiplication (a,b):
+    return 
+def division (a,b):
+ if b ==0:
+    
+    return "division zéro impossible"
+ return a/b
+
+n1= float(input("valeur premier nombre:"))
+n2= float(input("valeur deuxieme nombre:"))
